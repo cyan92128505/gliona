0ccafa0fa8bcad2392834058d17c97fbd9dbdf75
cyan92128505 <cyan92128505@gmail.com>
2020-11-08 15:45:52 +0800

    :wastebasket: Code clean

--- src/index.tsx
+++ src/index.tsx
@@ -15,10 +15,6 @@ const dataProvider = localStorageDataProvider({
       { id: 0, title: 'Hello, world!' },
       { id: 1, title: 'FooBar' },
     ],
-    comments: [
-      { id: 0, post_id: 0, author: 'John Doe', body: 'Sensational!' },
-      { id: 1, post_id: 0, author: 'Jane Doe', body: 'I agree' },
-    ],
   },
   localStorageKey: '',
   loggingEnabled: true,
