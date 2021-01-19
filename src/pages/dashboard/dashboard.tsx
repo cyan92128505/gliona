7cbca20b176b82a1727e016f173d4a00121580a3
cyan92128505 <cyan92128505@gmail.com>
2021-01-19 11:32:08 +0800

    :sparkles: Modify dashboard map detected location

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,27 +1,57 @@
 import 'leaflet/dist/leaflet.css';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core';
-import React from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet';
 import './dashboard.css';
 import { Title } from 'react-admin';
+import { Icon, LatLngExpression } from 'leaflet';
 import env from '../../config/env';
+import { MapMarker2X, MapMarkerShadow } from '../../components/shared/app_icons';
 
+const defaultPosition: LatLngExpression = [23.7393441, 120.4262101];
 const useStyles = makeStyles((theme) => ({
-  main: {
-    minHeight: '600px',
+  container: {
+    display: 'flex',
+    alignItems: 'stretch',
+    height: '100%',
+  },
+  item: {
+    width: '100%',
   },
 }));
+
+function LocationMarker() {
+  const [position, setPosition] = useState(defaultPosition);
+  const map = useMapEvents({
+    locationfound(e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+  useEffect(() => {
+    map.locate();
+  }, [map]);
+
+  const markerIcon = new Icon({
+    iconUrl: MapMarker2X,
+    shadowUrl: MapMarkerShadow,
+  });
+
+  return position === null ? null : (
+    <Marker icon={markerIcon} position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
+}
 
 function Dashboard() {
   const classes = useStyles();
   return (
-    <div>
+    <div className={classes.container}>
       <Title defaultTitle={env.appName?.toUpperCase()} />
-      <MapContainer className={classes.main} center={[23.7393441, 120.4262101]} zoom={10} scrollWheelZoom>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+      <MapContainer className={classes.item} center={defaultPosition} zoom={10} scrollWheelZoom>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <LocationMarker />
       </MapContainer>
     </div>
   );
